dc02e5c60b88de640ebcfc1cc3ce52ac271f58ac
omar imtiaz <oaimtiaz@utexas.edu>
2020-10-28 11:49:51 -0500

    Added notes to FM algorithm

--- src/foolsmate/foolsmate.rs
+++ src/foolsmate/foolsmate.rs
@@ -1,4 +1,3 @@
-
 //pub struct FoolsMate { // (self: Craft, enemy:Craft)
 
 //impl FoolsMate
@@ -7,6 +6,7 @@
 
 //pub func withinInnerSphere(self:Craft, enemy:Craft) -> bool
 
+//Check if closest point on current path of enemy plane is within the sector?
 //pub func withinSector(self:Craft, enemy:Craft) -> bool
 
 //pub func evade(self:Craft, enemy:Craft) -> Vector<??>
